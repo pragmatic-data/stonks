9650eb42612e11a0575c562852bf4935904d843a
Roberto Zagni <rz70rz70@gmail.com>
2025-03-01 09:58:20 +0000

    Changed macro to be able to view generated code with compile.

--- analyses/aaa_setup/setup_inspection.sql
+++ analyses/aaa_setup/setup_inspection.sql
@@ -1,25 +1,26 @@
 {%- set prj_name = var('project_short_name', 'STONKS') -%}
 {%- set useradmin_role = var('useradmin_role', 'USERADMIN') -%}
 
-------------
 {{ project_initial_setup__sql() }}
-------------
+
+/*
 {#{ sf_project_admin.create_dbt_executor_user(
         prj_name = prj_name,
         user_name = get_STONKS_user_dictionary().dbt_executor,
         useradmin_role = useradmin_role
 ) }#}
 ------------
-{{ sf_project_admin.create_users_from_dictionary(
+{#{ sf_project_admin.create_users_from_dictionary(
         prj_name, get_STONKS_user_dictionary(), useradmin_role
-) }}
+) }#}
 ------------
-{{ sf_project_admin.refresh_user_roles( 
+{#{ sf_project_admin.refresh_user_roles( 
         prj_name, 
         get_STONKS_user_dictionary(), 
         useradmin_role
-)  }}
+)  }#}
 ------------
 {#{ sf_project_admin.drop_users(
         get_STONKS_user_dictionary().users_to_delete, useradmin_role
 ) }#}
+*/
