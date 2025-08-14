28fd9dbebf285314def82b4cf8ff8b52e8af9e30
Roberto Zagni <rz70rz70@gmail.com>
2025-08-14 16:52:37 +0000

    Added script to load all sample files in the stage

---
+++ code_samples_by_chapter/chapter_09/util_scripts/put_ALL_files_to_stages.sql
@@ -0,0 +1,15 @@
+PUT 'file:///<path>/open_positions/.*OpenPositions.*.csv'
+    @STONKS_DEV.LAND_IB.IB_CSV__STAGE/open_positions/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///<path>/cash_transactions/.*CashTransactions.*.csv'
+    @STONKS_DEV.LAND_IB.IB_CSV__STAGE/cash_transactions/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///<path>/Trades/.*Trades.*.csv'
+    @STONKS_DEV.LAND_IB.IB_CSV__STAGE/trades/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
