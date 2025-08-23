3c9940effdc89897f7c3ae3971e536d69059ece8
Roberto Zagni <rz70rz70@gmail.com>
2025-08-23 15:47:02 +0000

    Added HIST and VER model

---
+++ models/01_storage/interactive_brokers/trades/VER_IB_TRADES.sql
@@ -0,0 +1,7 @@
+{{ pragmatic_data.versions_from_history_with_multiple_versions(
+    history_rel             = ref('HIST_IB_TRADES'), 
+    key_column              = 'TRADE_HKEY',
+    diff_column             = 'TRADE_HDIFF',
+
+    version_sort_column     = 'EFFECTIVITY_DATE'
+) }}
