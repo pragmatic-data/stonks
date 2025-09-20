f0e7f22b0fe859818f70046010a47a196dd38bf0
Roberto Zagni <rz70rz70@gmail.com>
2025-09-20 10:48:28 +0000

    Added also the portfolio dimension

--- models/03_delivery/marts/portfolio_analysis/DIM_PORTFOLIOS.sql
+++ models/03_delivery/marts/portfolio_analysis/DIM_PORTFOLIOS.sql
@@ -1,22 +1,18 @@
 with
-portfolio_daily_value_dim as (
-    select distinct
-        portfolio_hkey
-        , broker_code
-        , client_account_code
-        , account_alias
-        , case when broker_code = 'IB' then 'Interactive Brokers'
-        end as broker_name
-    from {{ ref('TS_IB_REPORTED_POSITIONS_DAILY_VALUES') }}
+portfolio_base_dim as (
+    select *
+    from {{ ref('REF_PORTFOLIOS') }}
 )
 
 , portfolio_position_stats as (
     select
         portfolio_hkey
         , count(distinct position_hkey) as positions_in_portfolio
+        , min(effectivity_date) as first_activity_date
         , max(effectivity_date) as last_position_change
+        , datediff('day', last_position_change, CURRENT_DATE()) <= 30 AS is_active_last_30_days
+        , datediff('day', last_position_change, CURRENT_DATE()) <= 360 AS is_active_last_360_days
     from {{ ref('VER_IB_POSITIONS_CALCULATED') }}
-    where is_current and side != 'Closed'
     group by portfolio_hkey
 )
 
@@ -34,7 +30,7 @@ select
     p.*
     , ps.* exclude(portfolio_hkey)
     , vs.* exclude(portfolio_hkey)
-from portfolio_daily_value_dim as p
+from portfolio_base_dim as p
 inner join portfolio_daily_value_stats as vs
     on p.portfolio_hkey = vs.portfolio_hkey
 inner join portfolio_position_stats as ps
