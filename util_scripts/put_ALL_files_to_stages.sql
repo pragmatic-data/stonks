1c57d642eda64ba8eba34b49caea2a0655d26615
Roberto Zagni <rz70rz70@gmail.com>
2025-09-21 12:46:20 +0000

    Load sample data to all envs

--- util_scripts/put_ALL_files_to_stages.sql
+++ util_scripts/put_ALL_files_to_stages.sql
@@ -1,23 +1,69 @@
 /* Execute these commands on stonks project by running the command:
    SNOWSQL_PRIVATE_KEY_PASSPHRASE="..." snowsql -c stonks -f /Users/robertozagni/RobMcZag/stonks/util_scripts/put_ALL_files_to_stages.sql
 */
-PUT 'file:///Users/robertozagni/Downloads/ingestion_base/open_positions/*OpenPositions*.csv'
+
+/* DEV */
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/open_positions/*OpenPositions*.csv'
     @STONKS_DEV.LAND_IB.IB_CSV__STAGE/open_positions/
     AUTO_COMPRESS = TRUE
     OVERWRITE = FALSE
 ;
-PUT 'file:///Users/robertozagni/Downloads/ingestion_base/CashTransactions/*CashTransactions*.csv'
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/cash_transactions/*CashTransactions*.csv'
     @STONKS_DEV.LAND_IB.IB_CSV__STAGE/cash_transactions/
     AUTO_COMPRESS = TRUE
     OVERWRITE = FALSE
 ;
-PUT 'file:///Users/robertozagni/Downloads/ingestion_base/Trades/*Trades*.csv'
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/trades/*Trades*.csv'
     @STONKS_DEV.LAND_IB.IB_CSV__STAGE/trades/
     AUTO_COMPRESS = TRUE
     OVERWRITE = FALSE
 ;
-PUT 'file:///Users/robertozagni/Downloads/ingestion_base/Transfers/Transfers*.csv'
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/transfers/Transfers*.csv'
     @STONKS_DEV.LAND_IB.IB_CSV__STAGE/transfers/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+
+/* QA */
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/open_positions/*OpenPositions*.csv'
+    @STONKS_QA.LAND_IB.IB_CSV__STAGE/open_positions/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/cash_transactions/*CashTransactions*.csv'
+    @STONKS_QA.LAND_IB.IB_CSV__STAGE/cash_transactions/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/trades/*Trades*.csv'
+    @STONKS_QA.LAND_IB.IB_CSV__STAGE/trades/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/transfers/Transfers*.csv'
+    @STONKS_QA.LAND_IB.IB_CSV__STAGE/transfers/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+
+/* PROD */
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/open_positions/*OpenPositions*.csv'
+    @STONKS_PROD.LAND_IB.IB_CSV__STAGE/open_positions/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/cash_transactions/*CashTransactions*.csv'
+    @STONKS_PROD.LAND_IB.IB_CSV__STAGE/cash_transactions/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/trades/*Trades*.csv'
+    @STONKS_PROD.LAND_IB.IB_CSV__STAGE/trades/
+    AUTO_COMPRESS = TRUE
+    OVERWRITE = FALSE
+;
+PUT 'file:///Users/robertozagni/Downloads/sample_IB_files/transfers/Transfers*.csv'
+    @STONKS_PROD.LAND_IB.IB_CSV__STAGE/transfers/
     AUTO_COMPRESS = TRUE
     OVERWRITE = FALSE
 ;
